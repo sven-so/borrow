278f29d1158231db6d0c910d0bd69d3dada2959a
sven-so <ich@svensommerlade.de>
2020-10-08 12:12:20 +0200

    Updates

---
+++ precache-manifest.d3ce94375561a9088104fac2fb02e936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2654eaaffdf437dbf3cfbdaca9ba8ba5",
+    "url": "/borrow/index.html"
+  },
+  {
+    "revision": "1aa65f203a33b2f7af2e",
+    "url": "/borrow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0b56d017aead4c823a3",
+    "url": "/borrow/static/js/2.0d4fdc13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/borrow/static/js/2.0d4fdc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa65f203a33b2f7af2e",
+    "url": "/borrow/static/js/main.5a9cf64d.chunk.js"
+  },
+  {
+    "revision": "3e0fe1643500ee101f76",
+    "url": "/borrow/static/js/runtime-main.e4ab6427.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/borrow/static/media/logo.5d5d9eef.svg"
+  }
+]);
